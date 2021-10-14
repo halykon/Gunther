11b0dcf22d829b53e35e016e5ee64fc7ff925597
lena <lena.daxenbichler@gmx.at>
2021-10-14 21:23:01 +0200

    added rainbow sword

--- src/main/java/net/fabricmc/example/ExampleMod.java
+++ src/main/java/net/fabricmc/example/ExampleMod.java
@@ -1,6 +1,13 @@
 package net.fabricmc.example;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.SwordItem;
+import net.minecraft.item.ToolItem;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -10,11 +17,17 @@ public class ExampleMod implements ModInitializer {
 	// That way, it's clear which mod wrote info, warnings, and errors.
 	public static final Logger LOGGER = LogManager.getLogger("modid");
 
+	// an instance of our new item
+	// public static final RainbowSword FABRIC_ITEM = new RainbowSword(new FabricItemSettings().group(ItemGroup.MISC));
+
+	public static ToolItem RAINBOW_SWORD = new SwordItem(RainbowToolMaterial.INSTANCE, 5, -2.0F, new Item.Settings().group(ItemGroup.COMBAT));
+
 	@Override
 	public void onInitialize() {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
+		Registry.register(Registry.ITEM, new Identifier("tutorial", "rainbow_sword"), RAINBOW_SWORD);
 
 		LOGGER.info("Hello Fabric world!");
 	}
---
+++ src/main/java/net/fabricmc/example/RainbowToolMaterial.java
@@ -0,0 +1,40 @@
+package net.fabricmc.example;
+
+import net.minecraft.item.Items;
+import net.minecraft.item.ToolMaterial;
+import net.minecraft.recipe.Ingredient;
+
+public class RainbowToolMaterial implements ToolMaterial {
+
+    public static final RainbowToolMaterial INSTANCE = new RainbowToolMaterial();
+
+    @Override
+    public int getDurability() {
+        return 500;
+    }
+
+    @Override
+    public float getMiningSpeedMultiplier() {
+        return 0;
+    }
+
+    @Override
+    public float getAttackDamage() {
+        return 5.0F;
+    }
+
+    @Override
+    public int getMiningLevel() {
+        return 0;
+    }
+
+    @Override
+    public int getEnchantability() {
+        return 20;
+    }
+
+    @Override
+    public Ingredient getRepairIngredient() {
+        return Ingredient.ofItems(Items.IRON_INGOT);
+    }
+}
