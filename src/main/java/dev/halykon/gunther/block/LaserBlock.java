057ec80ee876aee1c927f9eb4929b5487ffbad94
lena <lena.daxenbichler@gmx.at>
2021-10-22 12:18:55 +0200

    add directional block

--- src/main/java/dev/halykon/gunther/block/LaserBlock.java
+++ src/main/java/dev/halykon/gunther/block/LaserBlock.java
@@ -3,12 +3,20 @@ package dev.halykon.gunther.block;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemPlacementContext;
+import net.minecraft.sound.SoundEvents;
+import net.minecraft.state.StateManager;
 import net.minecraft.state.property.BooleanProperty;
+import net.minecraft.state.property.Properties;
 import net.minecraft.text.LiteralText;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
+import net.minecraft.util.shape.VoxelShape;
+import net.minecraft.util.shape.VoxelShapes;
+import net.minecraft.world.BlockView;
 import net.minecraft.world.World;
 
 public class LaserBlock extends Block {
@@ -16,15 +24,27 @@ public class LaserBlock extends Block {
 
     public LaserBlock(Settings settings) {
         super(settings);
-        System.out.println("whatever");
+        setDefaultState(getStateManager().getDefaultState().with(ACTIVATED, false));
+        setDefaultState(this.stateManager.getDefaultState().with(Properties.HORIZONTAL_FACING, Direction.NORTH));
+    }
+
+    @Override
+    protected void appendProperties(StateManager.Builder<Block, BlockState> stateManager) {
+        stateManager.add(ACTIVATED);
+        stateManager.add(Properties.HORIZONTAL_FACING);
     }
 
     @Override
     public ActionResult onUse(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hit) {
-        if (!world.isClient) {
-            player.sendMessage(new LiteralText("Hello, world!"), false);
-        }
-
+        player.playSound(SoundEvents.BLOCK_RESPAWN_ANCHOR_CHARGE, 1, 1);
+        world.setBlockState(pos, state.with(ACTIVATED, true));
+        System.out.println(ACTIVATED);
         return ActionResult.SUCCESS;
     }
+
+    @Override
+    public BlockState getPlacementState(ItemPlacementContext ctx) {
+        return (BlockState)this.getDefaultState().with(Properties.HORIZONTAL_FACING, ctx.getPlayerFacing().getOpposite());
+    }
+
 }
