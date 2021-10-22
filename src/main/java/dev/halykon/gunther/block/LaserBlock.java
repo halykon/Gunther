0f24c40356ead5a1450048ebf1710e0c8fb66ea9
lena <lena.daxenbichler@gmx.at>
2021-10-22 16:59:38 +0200

    commit directional block before breaking everything again

--- src/main/java/dev/halykon/gunther/block/LaserBlock.java
+++ src/main/java/dev/halykon/gunther/block/LaserBlock.java
@@ -1,7 +1,6 @@
 package dev.halykon.gunther.block;
 
-import net.minecraft.block.Block;
-import net.minecraft.block.BlockState;
+import net.minecraft.block.*;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemPlacementContext;
 import net.minecraft.sound.SoundEvents;
@@ -19,13 +18,14 @@ import net.minecraft.util.shape.VoxelShapes;
 import net.minecraft.world.BlockView;
 import net.minecraft.world.World;
 
-public class LaserBlock extends Block {
+public class LaserBlock extends HorizontalFacingBlock {
     public static final BooleanProperty ACTIVATED = BooleanProperty.of("activated");
 
     public LaserBlock(Settings settings) {
         super(settings);
-        setDefaultState(getStateManager().getDefaultState().with(ACTIVATED, false));
-        setDefaultState(this.stateManager.getDefaultState().with(Properties.HORIZONTAL_FACING, Direction.NORTH));
+        this.setDefaultState((BlockState)((BlockState)((BlockState)this.stateManager.getDefaultState()).with(Properties.HORIZONTAL_FACING, Direction.NORTH)).with(ACTIVATED, false));
+        // setDefaultState(getStateManager().getDefaultState().with(ACTIVATED, false));
+        // setDefaultState(this.stateManager.getDefaultState().with(Properties.HORIZONTAL_FACING, Direction.NORTH));
     }
 
     @Override
@@ -35,16 +35,18 @@ public class LaserBlock extends Block {
     }
 
     @Override
-    public ActionResult onUse(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hit) {
-        player.playSound(SoundEvents.BLOCK_RESPAWN_ANCHOR_CHARGE, 1, 1);
-        world.setBlockState(pos, state.with(ACTIVATED, true));
-        System.out.println(ACTIVATED);
-        return ActionResult.SUCCESS;
+    public BlockState getPlacementState(ItemPlacementContext ctx) {
+        return (BlockState)this.getDefaultState().with(Properties.HORIZONTAL_FACING, ctx.getPlayerFacing().getOpposite());
     }
 
     @Override
-    public BlockState getPlacementState(ItemPlacementContext ctx) {
-        return (BlockState)this.getDefaultState().with(Properties.HORIZONTAL_FACING, ctx.getPlayerFacing().getOpposite());
+    public ActionResult onUse(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hit) {
+        player.playSound(SoundEvents.BLOCK_RESPAWN_ANCHOR_CHARGE, 1, 1);
+        world.setBlockState(pos, state.with(ACTIVATED, true));
+        // Direction direction = hit.getSide();
+        // Direction direction2 = direction.getAxis() == Direction.Axis.Y ? player.getHorizontalFacing().getOpposite() : direction;
+        // world.setBlockState(pos, (BlockState) Blocks.CARVED_PUMPKIN.getDefaultState().with(CarvedPumpkinBlock.FACING, direction2), Block.NOTIFY_ALL | Block.REDRAW_ON_MAIN_THREAD);
+        return ActionResult.SUCCESS;
     }
 
 }
