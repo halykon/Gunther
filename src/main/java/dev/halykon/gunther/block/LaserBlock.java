45e0a803ef3c26befb93752661b791a29449002c
lena <lena.daxenbichler@gmx.at>
2021-10-27 21:33:49 +0200

    i did something more

--- src/main/java/dev/halykon/gunther/block/LaserBlock.java
+++ src/main/java/dev/halykon/gunther/block/LaserBlock.java
@@ -1,5 +1,6 @@
 package dev.halykon.gunther.block;
 
+import dev.halykon.gunther.Gunther;
 import net.minecraft.block.*;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
@@ -9,19 +10,17 @@ import net.minecraft.item.ItemPlacementContext;
 import net.minecraft.sound.SoundEvents;
 import net.minecraft.state.StateManager;
 import net.minecraft.state.property.BooleanProperty;
+import net.minecraft.state.property.DirectionProperty;
 import net.minecraft.state.property.Properties;
 import net.minecraft.state.property.Property;
-import net.minecraft.text.LiteralText;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
 import net.minecraft.util.math.Vec3d;
-import net.minecraft.util.shape.VoxelShape;
-import net.minecraft.util.shape.VoxelShapes;
-import net.minecraft.world.BlockView;
 import net.minecraft.world.World;
+import org.lwjgl.system.CallbackI;
 
 public class LaserBlock extends HorizontalFacingBlock {
     public static final BooleanProperty ACTIVATED = BooleanProperty.of("activated");
@@ -45,11 +44,42 @@ public class LaserBlock extends HorizontalFacingBlock {
     @Override
     public ActionResult onUse(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hit) {
         player.playSound(SoundEvents.BLOCK_RESPAWN_ANCHOR_CHARGE, 1, 1);
+        System.out.println("------------------------------- POS:");
+        System.out.println(pos);
         if (world.getBlockState(pos).get(ACTIVATED)) {
             System.out.println("active");
         }
         world.setBlockState(pos, state.with(ACTIVATED, true));
         System.out.println("-------------------------------");
+
+        int current_x = pos.getX();
+        int current_y = pos.getY();
+        int current_z = pos.getZ();
+
+        if (world.getBlockState(pos).get(Properties.HORIZONTAL_FACING) == Direction.EAST) {
+            for (int i = 1; i <= 11; i++) {
+                int check_x = current_x + i;
+                BlockPos x = new BlockPos(check_x, current_y, current_z);
+                System.out.println(world.getBlockState(x).getBlock());
+
+                Block new_block = world.getBlockState(x).getBlock();
+                Block current_block = world.getBlockState(pos).getBlock();
+
+                if (new_block == current_block) {
+                    System.out.println("is same type ja");
+
+                    Boolean new_block_state = world.getBlockState(x).get(ACTIVATED);
+                    System.out.println(new_block_state);
+
+                    Direction direction = world.getBlockState(pos).get(Properties.HORIZONTAL_FACING);
+                    if (new_block_state) {
+                        System.out.println("is activate ja");
+                    } else {
+                        System.out.println("is deactivate sadge");
+                    }
+                }
+            }
+        }
 
         if (world.getBlockState(pos).get(Properties.HORIZONTAL_FACING) == Direction.NORTH) {
             System.out.println(Direction.NORTH);
